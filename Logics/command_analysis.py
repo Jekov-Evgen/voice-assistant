f7c8a6dee73e982c0884c00627d5df33e27b4d6c
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-06 01:47:40 +0200

    add command Tomorrow we will start writing the interface

--- Logics/command_analysis.py
+++ Logics/command_analysis.py
@@ -1,8 +1,19 @@
 import os
+import webbrowser
+from time import sleep
 
 class Analysis:
     def analyze(self, text : str):
-        command = {"привет": self.greet, "ладонь" : self.five, "интернет" : self.open_google}
+        command = {"привет": self.greet, 
+                   "ладонь" : self.five, 
+                   "интернет" : self.open_google, 
+                   "записать": self.create_notebook,
+                   "проекты": self.open_my_git,
+                   "алгоритмы" : self.open_my_codewars, 
+                   "видео" : self.open_my_youtube,
+                   "код" : self.open_vs_cod,
+                   "погода" : self.get_weather,
+                   "таймер" : self.timer}
         
         if text in command:
             command[text]()
@@ -15,3 +26,28 @@ class Analysis:
         
     def open_google(self):
         os.startfile(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+        
+    def create_notebook(self):
+        fl = open("test.txt", 'w')
+        os.startfile(r"C:\Users\Женя\OneDrive\Desktop\programming\ML\my ml\test.txt")
+        
+    def open_my_git(self):
+        webbrowser.open("https://github.com/Jekov-Evgen")
+        
+    def open_my_codewars(self):
+        webbrowser.open("https://www.codewars.com/users/jeeeek")
+        
+    def open_my_youtube(self):
+        webbrowser.open("https://www.youtube.com/")
+        
+    def open_vs_cod(self):
+        os.startfile(r"C:\Users\Женя\AppData\Local\Programs\Microsoft VS Code\Code.exe")
+        
+    def get_weather(self):
+        webbrowser.open(r"https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BE%D0%B4%D0%B5%D1%81%D0%B0")
+        
+    def timer(self):
+        print("введите время для таймера: ")
+        tm = int(input())
+        sleep(tm)
+        print("время вышло")
